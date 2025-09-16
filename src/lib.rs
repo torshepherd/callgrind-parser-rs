488495f5ab6d286eca14cf8f574aaaef4734fde8
Tor Shepherd <tor.aksel.shepherd@gmail.com>
2025-09-16 17:57:19 -0400

    unfinished

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use pyo3::prelude::*;
+
+mod parser;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn callgrind_parser_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_submodule(parser::parser(m)?)?;
+    Ok(())
+}
